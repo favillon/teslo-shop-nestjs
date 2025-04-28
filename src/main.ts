49cccd220130c7720ea9948ac46ca570af20289d
favillon <favillon@hotmail.com>
2025-04-28 13:01:43 -0500

    Agregando documentacion en swagger v2 correcion textos

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Teslo Shop API')
+    .setDescription('The Teslo shop API description')
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
